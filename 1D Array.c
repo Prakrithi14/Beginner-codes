1198650b343b7e96be45108076bff9ff423ca4de
Prakrithi14 <154612957+Prakrithi14@users.noreply.github.com>
2024-08-15 18:56:23 +0530

    1D Array

---
+++ 1D Array.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main( )
+{
+  int arr[5] = {10, 20, 30, 40, 50};	//array declaration and initialization int i;
+  printf(“Elements of the array are:\n”); 
+  for(i=0; i<5; i++)
+  {
+    printf(“%d”, arr[i]);
+  }
+}
