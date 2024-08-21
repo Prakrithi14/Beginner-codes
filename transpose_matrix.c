8e55048b3b581589caf4603cf1b547a19d6e7085
Prakrithi14 <154612957+Prakrithi14@users.noreply.github.com>
2024-08-21 21:18:21 +0530

    Create transpose_matrix

---
+++ transpose_matrix.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+void main()
+{
+  int array[10][10]; 
+  int i, j, m, n;
+  printf("Enter the order of the matrix \n"); 
+  scanf("%d %d", &m, &n);
+  printf("Enter the coefficients of the matrix\n");
+  for (i = 0; i < m; ++i)
+  {
+    for (j = 0; j < n; ++j)
+    {
+      scanf("%d", &array[i][j]);
+    }
+  printf("\n");
+  }
+  printf("Transpose of matrix is \n"); 
+  for (j = 0; j < n; ++j)
+  {
+    for (i = 0; i < m; ++i)
+    {
+      printf(" %d", array[i][j]);
+    }
+    printf("\n");
+  }
+}
